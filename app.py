36d8da2e9f8cca3b7218134d89f961b35bb5d4d0
superbakuryu <superbakuryu@gmail.com>
2021-05-04 10:02:54 +0700

    Edit menu

--- app.py
+++ app.py
@@ -43,20 +43,6 @@ def shorten_id(value):
     return abs(hash(value)) % (10 ** 8)
 
 
-@app.context_processor
-def check_service_registered():
-    def _check_service_registered(client_id, service_id):
-        if not isinstance(client_id, ObjectId):
-            client_id = ObjectId(client_id)
-        find_client = mydb.clients.find_one({"_id": client_id})
-        register_services = find_client.get('services')
-        for x in register_services:
-            if x['service_id'] == service_id:
-                return 1
-        return 0
-    return dict(check_service_registered=_check_service_registered)
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -67,22 +53,12 @@ def before_request():
     g.user = None
 
     if 'user_name' in session:
-        if mydb.users.find_one({'username': session['user_name']}):
-            g.user = mydb.users.find_one({'username': session['user_name']})
-        elif mydb.supporters.find_one({'email': session['user_name']}):
-            g.user = mydb.supporters.find_one({'email': session['user_name']})
-        elif mydb.partners.find_one({'email': session['user_name']}):
+        if mydb.partners.find_one({'email': session['user_name']}):
             g.user = mydb.partners.find_one({'email': session['user_name']})
         elif mydb.clients.find_one({'email': session['user_name']}):
             g.user = mydb.clients.find_one({'email': session['user_name']})
-
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if not g.user:
-        return redirect(url_for('login'))
-
-    return render_template('dashboard.html', user_name=session['user_name'], role=session['role'], id=session['id'])
+            g.services_registered = api.get_service_registered(session['id'])
+            g.services_not_registered = api.get_services_not_registered(session['id'])
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -93,31 +69,14 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        if mydb.users.find_one({'username': username, 'password': password}):
-            session['user_name'] = username
-            session['id'] = str(mydb.users.find_one(
-                {'username': username, 'password': password}).get('_id'))
-            print(1111111111)
-            session['role'] = 'admin'
-            return redirect(url_for('dashboard'))
-        elif mydb.supporters.find_one({'email': username}):
-            print(222222222222)
-            if (api.verify_password(password, mydb.supporters.find_one({'email': username}).get('password'))):
-                session['user_name'] = username
-                session['id'] = str(mydb.supporters.find_one(
-                    {'email': username}).get('_id'))
-                session['role'] = 'supporter'
-                return redirect(url_for('dashboard'))
-            else:
-                return render_template('login.html')
-        elif mydb.partners.find_one({'email': username}):
+        if mydb.partners.find_one({'email': username}):
             print(3333333333)
             if (api.verify_password(password, mydb.partners.find_one({'email': username}).get('password'))):
                 session['user_name'] = username
                 session['id'] = str(mydb.partners.find_one(
                     {'email': username}).get('_id'))
                 session['role'] = 'partner'
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('index'))
             else:
                 return render_template('login.html')
         elif mydb.clients.find_one({'email': username}):
@@ -127,7 +86,7 @@ def login():
                 session['id'] = str(mydb.clients.find_one(
                     {'email': username}).get('_id'))
                 session['role'] = 'client'
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('index'))
             else:
                 return render_template('login.html')
         else:
@@ -136,20 +95,20 @@ def login():
         return render_template('login.html')
 
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('index.html', user_name=session['user_name'], role=session['role'], id=session['id'],services_registered = g.services_registered, services_not_registered = g.services_not_registered)
+
+
 @app.route('/index', methods=['GET'])
 def index():
     if not g.user:
         return redirect(url_for('login'))
 
-    return render_template('index.html', user_name=session['user_name'])
-
-
-@app.route('/dashboard', methods=['GET'])
-def dashboard():
-    if not g.user:
-        return redirect(url_for('login'))
-
-    return render_template('dashboard.html', user_name=session['user_name'], role=session['role'], id=session['id'])
+    return render_template('index.html', user_name=session['user_name'], role=session['role'], id=session['id'],services_registered = g.services_registered, services_not_registered = g.services_not_registered)
 
 
 # CLIENT
@@ -160,7 +119,7 @@ def manage_clients():
     if not g.user:
         return redirect(url_for('login'))
     if session['role'] == 'client':
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('index'))
     if session['role'] == 'admin' or session['role'] == 'supporter':
         clients = mydb.clients.find()
     else:
@@ -173,7 +132,7 @@ def client_add():
     if not g.user:
         return redirect(url_for('login'))
     if session['role'] == 'client':
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('index'))
     if request.method == 'POST':
         inputName = request.form['inputName']
         inputEmail = request.form['inputEmail']
@@ -235,7 +194,7 @@ def client_edit(client_id):
     if not g.user:
         return redirect(url_for('login'))
     if session['role'] == 'client':
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('index'))
     if request.method == 'POST':
         inputName = request.form['inputName']
         inputEmail = request.form['inputEmail']
@@ -303,7 +262,7 @@ def client_delete(client_id):
     if not g.user:
         return redirect(url_for('login'))
     if session['role'] == 'client':
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('index'))
     find_by_id = api.get_client_info(client_id)
     return render_template('client_delete.html', user_name=session['user_name'], id=session['id'], role=session['role'], client=find_by_id)
 
@@ -338,6 +297,17 @@ def register_service(client_id):
     return render_template('register_service.html', user_name=session['user_name'], id=session['id'], role=session['role'], services=find_service)
 
 
+@app.route('/service_info/<service_id>', methods=['GET', 'POST'])
+def service_info(service_id):
+    if not g.user:
+        return redirect(url_for('login'))
+    # if session['role'] == 'client':
+    #     find_service = api.get_service_registered(client_id)
+    # elif session['role'] == 'partner':
+    #     find_service = api.get_service_registered_partner(client_id)
+    find_service = api.get_service_info(service_id)
+    return render_template('service_info.html', user_name=session['user_name'], role=session['role'], service=find_service, id=session['id'],services_registered = g.services_registered, services_not_registered = g.services_not_registered)
+
 @app.route('/blank', methods=['GET'])
 def blank():
     if not g.user:
@@ -353,5 +323,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    # app.run(host='127.0.0.1', port=8099, debug=True)
-    serve(app, host='0.0.0.0', port=9197)
+    app.run(host='127.0.0.1', port=8099, debug=True)
+    # serve(app, host='0.0.0.0', port=9197)
